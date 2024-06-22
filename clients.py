253030aa453dbf9f7a4fa0cb4b89ad88dfbad451
Simon Nunn <simon.nunn@gmail.com>
2024-06-22 08:39:01 +0100

    Thread locked

--- clients.py
+++ clients.py
@@ -7,15 +7,16 @@ from argparse import ArgumentParser
 from random import randint
 import atexit
 
+packet_mutex = Lock()
+reset_mutex = Lock()
+session_mutex = Lock()
+
 global session
 session = Session()
-
-packet_mutex = Lock()
 
 global packet_number
 packet_number=0
 
-reset_mutex = Lock()
 global reset_counter
 reset_counter=0
 
@@ -24,22 +25,25 @@ def increment_reset_counter():
     global reset_counter
     with reset_mutex:
         reset_counter = reset_counter + 1
+    return reset_counter
 
 
 def get_session():
     global session
-    if session is None:
-        print("Session Started")
-        session = Session()
+    with session_mutex:
+        if session is None:
+            print("Session Started")
+            session = Session()
     return session
 
 
 def reset_session():
     global session
-    if session is not None:
-        print("Session Reset")
-        increment_reset_counter()
-        session.close()
+    with session_mutex:
+        if session is not None:
+            reset_number = increment_reset_counter()
+            print(f"Session Reset #{reset_number}")
+            session.close()
 
 
 def get_packet_number():
@@ -49,10 +53,9 @@ def get_packet_number():
     return packet_number
 
 
-def exec(session, timeout=3):
-    # sleep(randint(1,5))
+def exec(timeout=3):
     try:
-        response = session.get("http://127.0.0.1:8080",
+        response = get_session().get("http://127.0.0.1:8080",
                                data=str(get_packet_number()),
                                timeout=timeout)
         print(response.text)
@@ -65,18 +68,15 @@ def proc(timeout=3):
     while True:
         # sleep(1)
         t = Thread(target=exec,
-                   args=[get_session(),
-                         timeout])
+                   args=[timeout])
         t.start()
 
 
 def exit_handler():
-    sleep(1)
     global reset_counter
-    print (f"==========")
-
-    print (f"resets: {reset_counter}")
-    print (f"==========")
+    print (f"==================")
+    print (f"|| resets: {reset_counter} ||")
+    print (f"==================")
 
 
 if __name__=="__main__":
@@ -93,5 +93,4 @@ if __name__=="__main__":
     proc2.start()
 
     sleep(10)
-    Thread(target=exit_handler())
     _exit(0)
